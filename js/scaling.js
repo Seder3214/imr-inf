b56952ac49d69e9a1ded0881284248ae3fa0a4c9
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-06-29 13:54:19 +0300

    fix [29.06]

--- js/scaling.js
+++ js/scaling.js
@@ -336,7 +336,7 @@ function getScalingStart(type, name) {
 			if (hasBeyondRank(5,2)) start = start.add(beyondRankEffect(5,2,0))
 			if (hasBeyondRank(8,2)) start = start.add(beyondRankEffect(8,2))
 		}
-if (hasElement(252) && name !== ( 'FSS' || 'inf_theorem)) start = start.add(elemEffect(252))
+if (hasElement(252) && name !== ( 'FSS' || 'inf_theorem')) start = start.add(elemEffect(252))
 	}
 	else if (type==1) {
 		if (name=="tickspeed") {
