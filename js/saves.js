b8271810ae0632a88e6ac878e347c876a6e76926
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-06-29 17:35:40 +0300

    new beta-tester

--- js/saves.js
+++ js/saves.js
@@ -4,6 +4,7 @@ const FPS = 20
 let tester1 = btoa(JSON.stringify('256815'))
 let tester2 = btoa(JSON.stringify('472638'))
 let tester3 = btoa(JSON.stringify('364173'))
+let tester4 = btoa(JSON.stringify('517835'))
 function uni(x) { return E(1.5e56).mul(x) }
 function mlt(x) { return uni("ee9").pow(x) }
 
@@ -511,7 +512,7 @@ function importy() {
 function enterBeta() {
     createPrompt("To enter beta, type your beta tester id!",'import',loadbeta=>{
        let s = convertStringIntoLink('link')
-        if (loadbeta == (tester1 || tester2 || tester3)) {
+        if (loadbeta == (tester1 || tester2 || tester3 || tester4)) {
             window.location.replace(s);
         }
         else createPrompt("The id is not correct! Try again!")
