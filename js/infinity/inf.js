f9e2ab84cbd20f0ea6a16cf6dbc4ba2b6cf8e0fe
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-12 01:37:32 +0300

    Update inf.js

--- js/infinity/inf.js
+++ js/infinity/inf.js
@@ -17,8 +17,8 @@ const INF = {
         if (hasInfUpgrade(3)) e.push(161)
         if (iu15) e.push(218)
 
-        for (let i = 0; i < player.atom.elements.length; i++) if (player.atom.elements[i] > 218) e.push(player.atom.elements[i])
-
+          if (!player.chal.active == 19){ for (let i = 0; i < player.atom.elements.length; i++) if (player.atom.elements[i] > 218) e.push(player.atom.elements[i])
+}
         player.atom.elements = e
 let me = []
         if (hasElement(40,1)) for (let i = 0; i < player.atom.muonic_el.length; i++) me.push(player.atom.muonic_el[i])
