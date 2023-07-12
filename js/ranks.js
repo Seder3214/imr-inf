df1be0639b5347a53c86b8b1c6b301f1db48afc8
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-12 11:16:06 +0300

    Update ranks.js

--- js/ranks.js
+++ js/ranks.js
@@ -307,7 +307,7 @@ const PRESTIGES = {
             let br = E(tmp.beyond_ranks.max_tier)
             if (hasBeyondPres(1,2)) x = x.add(br).mul(r.add(1))
             if (hasPrestige(0,18) && i == 0) r = r.mul(2)
-           else x = x.mul(r.add(1))
+            x = x.mul(r.add(1))
         }
 
         if (tmp.dark.abEff.pb) x = x.mul(tmp.dark.abEff.pb)
