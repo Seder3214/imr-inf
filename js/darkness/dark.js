84e255ba7e1ba89f702891beef8bfbdab594ded1
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-25 00:34:07 +0300

    Update dark.js

--- js/darkness/dark.js
+++ js/darkness/dark.js
@@ -160,9 +160,8 @@ const DARK = {
         if (a.gte('e56000') && !tmp.c16active) x.ApQ_Overflow = Decimal.pow(10,a.div('e56000').log10().add(1).log10())
         if (a.gte('e125500')) x.fss = a.div('e56000').log10().add(1).log10().div(10).add(1).toNumber()
         if (a.gte('ee7')) {
-            x.ea = a.div('ee7').log10().div(1e6).add(1).root(2).softcap(hasElement(31,1)?3:1.75,0.25,0).add(hasElement(312)?elemEffect(312):0)
+            x.ea = a.div('ee7').log10().div(1e6).add(1).root(2).softcap(hasElement(31,1)?3:1.75,0.25,0).add(hasElement(312)?elemEffect(312):0).softcap(10,0.1,0)
         }
-x.ea = overflow(x.ea,10,0.1)
 
         return x
     },
