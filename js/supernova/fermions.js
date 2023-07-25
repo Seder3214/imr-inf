d043d070f76914b485f43d99998b306e597b0f83
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-25 20:37:10 +0300

    Update fermions.js

--- js/supernova/fermions.js
+++ js/supernova/fermions.js
@@ -256,7 +256,7 @@ const FERMIONS = {
                     return x = overflow(x,1.65,0.1)
                 },
                 desc(x) {
-                    return `Meta-Fermions starts ^${x.format()} later`
+                    return `Meta-Fermions and Exotic-Tier starts ^${x.format()} later`
                 },
                 inc: "mass",
                 cons: "Max Theorem's level is 0 and all u-quarks at once, and force infinity reset.",
