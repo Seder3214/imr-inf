2613e6e599c86e5792dde288026ae812e875679f
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-27 01:15:26 +0300

    Update md.js

--- js/md.js
+++ js/md.js
@@ -166,6 +166,7 @@ const MASS_DILATION = {
                 cost(x) { return E(5).pow(x).mul('1.50001e536') },
                 bulk() { return player.md.mass.gte('1.50001e536')?player.md.mass.div('1.50001e536').max(1).log(5).add(1).floor():E(0) },
                 effect(x) {
+x = overflow(x,'ee1500',0.1)
                     return E(2).pow(x).softcap(1e25,2/3,0)//.softcap("ee12",0.8,2)
                 },
                 effDesc(x) { return format(x)+"x"+(x.gte(1e25)?" <span class='soft'>(softcapped)</span>":"") },
