6dba62ef08572ed20a168fd46d58ab071cefe084
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-11-01 12:29:31 +0300

    final

--- js/multiverse/circle.js
+++ js/multiverse/circle.js
@@ -291,7 +291,7 @@ const SPELL = {
 
     function circleEffects() {
         let a = E(tmp.mv.cycleGain), eff = {}
-        eff.cs = expMult(a.div(10).add(1), a.div(10).add(1).log10().add(1))
+        eff.cs = Decimal.pow(a.add(1).div(10),a.div(2))
        if (hasElement(44,1)) eff.theoremLvl = a.add(1).pow(1.75).root(2).floor()
         return eff
     }
