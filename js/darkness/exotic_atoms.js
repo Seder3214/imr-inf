cfa4b35095455b309d81ddc7855caaf2331f4fdf
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-11-03 13:46:02 +0300

    Update exotic_atoms.js

--- js/darkness/exotic_atoms.js
+++ js/darkness/exotic_atoms.js
@@ -366,6 +366,10 @@ const MUONIC_ELEM = {
             mlt: true,
             desc: `Increase the amount of cycles needed to get Orbits based on unlocked circle' effects amount.`,
             cost: E(183000),
+eff() {let x = E(0)
+x = E(2).mul(1.15).round()
+return x},
+effDesc: x=>"+"+format(x),
         },
         {
             mlt: true,
